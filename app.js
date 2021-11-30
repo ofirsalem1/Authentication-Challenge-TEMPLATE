d78fc159ed3a2a777962455585087487425eac3b
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-11-30 15:58:44 +0200

    adding register user to INFORMATION and USERS

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-/* write your server code here */
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// { email: "admin@email.com", name: "admin", password:"**hashed password**", isAdmin: true }.//the password must be:Rc123456!
+const USERS = [{ email: 'admin@email.com', name: 'admin', password: '**hashed password**', isAdmin: true }];
+const INFORMATION = []; // {email: ${email}, info: "${name} info"}
+const REFRESHTOKENS = [];
+
+router.post('/users/register', async (req, res) => {
+  try {
+    const { email, user, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    if (findIfExists(email)) {
+      res.status(409).send('user already exists');
+    } else {
+      USERS.push({ email, name: user, password: hashedPassword, isAdmin: false });
+      INFORMATION.push({ email, info: `${user} info` });
+      res.status(201).send('Register Success');
+    }
+    console.log('INFORMATION', INFORMATION);
+    console.log('USERS', USERS);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/users/login', (req, res) => {
+  const { email, password } = req.body;
+  res.status(200).send('accessToken, refreshToken , email, name, isAdmin');
+});
+
+module.exports = router;
+
+const findIfExists = email => {
+  for (let i of INFORMATION) {
+    if (i.email === email) {
+      return true;
+    }
+  }
+  return false;
+};
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-/* write the code to run app.js here */
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const appFile = require('./app');
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/', appFile);
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});

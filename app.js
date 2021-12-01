fd21569ff3a18c512c0ea373291a2fde8243069f
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2021-12-01 15:48:08 +0200

    add options path

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const userRout = require('./routers/usersRout');
 const apiRout = require('./routers/apiRout');
 app.use(express.json());
-const { tokenValidateFunc } = require('./controller/users');
 const serverApis = [
   { method: 'post', path: '/users/register', description: 'Register, Required: email, name, password', example: { body: { email: 'user@email.com', name: 'user', password: 'password' } } },
   { method: 'post', path: '/users/login', description: 'Login, Required: valid email and password', example: { body: { email: 'user@email.com', password: 'password' } } },
@@ -16,8 +16,21 @@ const serverApis = [
 app.use('/users', userRout);
 app.use('/api', apiRout);
 
-app.options('/', tokenValidateFunc, (req, res) => {
-  res.header({ Allow: 'OPTIONS, GET, POST' }).send(serverApis);
+app.options('/', (req, res) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.header({ Allow: 'OPTIONS, GET, POST' }).send([serverApis[0], serverApis[1]]);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.header({ Allow: 'OPTIONS, GET, POST' }).send([serverApis[0], serverApis[1], serverApis[2]]);
+    }
+    if (user.isAdmin) {
+      return res.header({ Allow: 'OPTIONS, GET, POST' }).send(serverApis);
+    }
+    return res.header({ Allow: 'OPTIONS, GET, POST' }).send([serverApis[0], serverApis[1], serverApis[2], serverApis[3], serverApis[4], serverApis[5]]);
+  });
 });
 
 module.exports = app;
